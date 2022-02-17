4e507b6f6f6c41c0288eb981c29f0e23823e3092
Peter Arriaza <peter.arriaza@gmail.com>
2022-02-16 19:46:00 -0500

    added chromatic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,16 @@ import { TaskModule } from './components/task.module';
 import { NgxsModule } from '@ngxs/store';
 import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
 import { NgxsLoggerPluginModule } from '@ngxs/logger-plugin';
-
+import { InboxScreenComponent } from './components/inbox-screen/inbox-screen.component';
+import { PureInboxScreenComponent } from './components/inbox-screen/pure-inbox-screen.component';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InboxScreenComponent,
+    PureInboxScreenComponent
   ],
   imports: [
     BrowserModule,
